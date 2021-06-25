2744c788625b04e87c12a90d1c18a5f9bf17b5b7
Simone Ballard <simone.a.ballard@gmail.com>
2021-06-24 23:13:28 -0600

    email form added but not currently configued to her gmail

---
+++ main/src/components/Contact.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Nav from './Nav';
+import EmailForm from './EmailForm';
+
+function Contact() {
+    return (
+        <div>
+           <EmailForm 
+           /> 
+        </div>
+    )
+}
+
+export default Contact
