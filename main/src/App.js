faf2e05530e848926f9d99f33410263043cd2699
Simone Ballard <simone.a.ballard@gmail.com>
2021-06-24 23:03:55 -0600

    nav will follow standard format

--- main/src/App.js
+++ main/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+//Components
+import CV from './components/CV'
+import Contact from './components/Contact'
+import About from './components/About'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+   <BrowserRouter>
+   <div className="App">
+     <Switch>
+       <Route exact path='/contact' component={Contact} />
+       <Route exact path='/cv' component={CV} />
+       <Route exact path='/about' component={About} />
+     </Switch>
+   </div>
+   </BrowserRouter>
+   </>
   );
 }
 
--- main/src/components/Nav.js
+++ main/src/components/Nav.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
-function Nav() {
+function Nav({ firstTxt, firstLink, twoTxt, twoLink, threeTxt, threeLink, fourTxt, fourLink }) {
     return (
-        <div>
-            
+        <div className='main-nav'>
+            <button text={firstTxt} link={firstLink} />
+            <button text={twoTxt} link={twoLink} />
+            <button text={threeTxt} link={threeLink} />
+            <button text={fourTxt} link={fourLink} />
         </div>
     )
 }
