fd2fa2ca576d3e95682468a27fcbb0d741831748
Simone Ballard <simone.a.ballard@gmail.com>
2021-06-26 01:30:53 -0600

    home page text updated

--- main/src/components/Home.js
+++ main/src/components/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Nav from './Nav'
+import Profile from '../assets/Lucy2.jpg';
 
 import '../styles/Home.css'
 
@@ -14,8 +15,10 @@ function Home() {
                 threeTxt='Contact'
                 threeLink='/contact'
             />
-            <div className="text">
-                <p></p>
+            <div className="home-contents">
+                <img src={Profile} alt="Lucija">
+                </img>
+                <p>Hi! My name is Lucija and I'm a native Croatian who followed her passion for music, psychology, and experience around the world. I'm currently enhancing my training in programming, advanced statistics, and HCI confidently seeking job opportunities in User Experience research and Data Analyst careers. From my diverse experience and psychology background I hold a strong research aptitude, analytical mindset and am adept at working both as a cross-functional team member and individual contributor.</p>
             </div>
         </div>
     )
